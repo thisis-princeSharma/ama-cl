45617b92a4132de765446f218630992942ef08b8
PrinceSharma <121371635+thisis-princeSharma@users.noreply.github.com>
2024-06-15 23:49:21 +0000

    Made side bar

--- wh-cl/src/Chat.js
+++ wh-cl/src/Chat.js
@@ -1,40 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import './Chat.css'
-import { Avatar, IconButton } from '@mui/material';
-import SearchOutlined from '@mui/icons-material/SearchOutlined';
-import AttachFile from '@mui/icons-material/AttachFile';
-import MoreVert from '@mui/icons-material/MoreVert';
-
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@mui/material";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import AttachFile from "@mui/icons-material/AttachFile";
+import MoreVert from "@mui/icons-material/MoreVert";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
+import SelectInput from "@mui/material/Select/SelectInput";
 
 function Chat() {
-    const [seed, setSeed] = useState("")
-    useEffect(() => {
-        setSeed(Math.floor(Math.random() * 5000))
-    }, []);
+  const [input, setInput] = useState("");
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+
+  const sendMessage = (e) => {
+    e.preventDefault(); 
+    console.log('You typed input')
+  };
+
   return (
-    <div className='chat'>
-        <div className='chat_header'>
-            <Avatar src={`https://api.dicebear.com/8.x/adventurer-neutral/svg?seed="${seed}"`}/>
-            <div className='header_info'>
-                <h3>Room name</h3>
-                <p>Last seen at...</p>
-            </div>
-            <div className='chat_headerRight'>
-                <IconButton>
-                    <SearchOutlined/>
-                </IconButton>
-                <IconButton>
-                    <AttachFile />
-                </IconButton>
-                <IconButton>
-                    <MoreVert/>
-                </IconButton>
-            </div>
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar
+          src={`https://api.dicebear.com/8.x/adventurer-neutral/svg?seed="${seed}"`}
+        />
+        <div className="header_info">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
-        <div className='chat_body'></div>
-        <div className='chat_footer'></div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        <p className={`chat_message ${true && "chat_reciever"}`}>
+          <span className="chat_name">PrinceSharma</span>
+          Hi
+          <span className="chat_timestamp">3:50pm</span>
+        </p>
+      </div>
+      <div className="chat_footer">
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+        <form>
+          <input value={input} onChange={e => setInput(e.target.value)} onClick={sendMessage} placeholder="Type a message" type="submit"/>
+          <button>Send Message</button>
+          <IconButton>
+            <MicIcon />
+          </IconButton>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
