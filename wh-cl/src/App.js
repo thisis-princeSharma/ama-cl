1b120ea4376eca123de82e4e215144f0325e2149
PrinceSharma <121371635+thisis-princeSharma@users.noreply.github.com>
2024-06-15 18:27:31 +0000

    Added sidebar

--- wh-cl/src/App.js
+++ wh-cl/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='app_body'>
+        {/* sidebar */}
+        {/* chat area */}
+      </div>
     </div>
   );
 }
---
+++ wh-cl/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC2klFnIe5K1Mi3lYmU3EauOXMQn66YV6k",
+    authDomain: "wha-cl.firebaseapp.com",
+    projectId: "wha-cl",
+    storageBucket: "wha-cl.appspot.com",
+    messagingSenderId: "324454899961",
+    appId: "1:324454899961:web:66ed7d92e635d7ce658228",
+    measurementId: "G-M241YC1KYX"
+};g
